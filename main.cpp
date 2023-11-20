1beca25cce8327315c7db3301932ecd530b0053c
Kirill <hickkan008@gmail.com>
2023-11-20 16:14:33 +0300

    modified:   main.cpp 	modified:   main.exe

--- main.cpp
+++ main.cpp
@@ -13,14 +13,10 @@ private:
 };
 
 int main(){
-    
-    std::string input;
-    std::cin >> input;
 
-    //WinExec("C:\\osu!\\osu!.exe", 1);
-    //return 0;
-
-    std::cout << input;
-    std::cout << "Code is end" << std::endl;
+    WinExec("C:\\osu!\\osu!.exe", 1);
+    return 0;
+ 
+    std::cout << "code execution" << std::endl;
 
 }
