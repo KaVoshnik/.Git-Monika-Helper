a720f48343c79a4130b71c279e0eaee52523d53d
Kirill <hickkan008@gmail.com>
2023-11-20 13:27:04 +0300

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,6 @@
 #include <fstream>
 #include <Windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
-				   PSTR cmdLine, int showCmd)
-
-{
-	WinExec( "C:\\osu!\\osu!.exe", 1);
-	return 0;
-}
 class monika
 {
 public:
@@ -22,6 +15,8 @@ private:
 int main(){
 
     std::cout << "hello world" << std::endl;
-
+    
+    WinExec("C:\\osu!\\osu!.exe", 1);
+    return 0;
 
 }
