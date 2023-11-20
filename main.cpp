e58a455f8d39d757c0d193b64b4c77af2bd71658
Kirill <hickkan008@gmail.com>
2023-11-20 13:00:15 +0300

    new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+class monika
+{
+public:
+    std::string name = "monika";
+private:
+
+};
+
+int main(){
+
+    std::cout << "hello world" << std::endl;
+
+}
