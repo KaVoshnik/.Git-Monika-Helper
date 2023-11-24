bd8f03f3d58939d068651fd9a039dd249b245e61
Kirill <hickkan008@gmail.com>
2023-11-24 15:04:51 +0300

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -20,19 +20,7 @@ private:
 Helper(std::string _P_name, std::string _H_name, int _old) :
         P_name(_P_name), H_name(_H_name), old(_old) {}
 };
-
-void printTable(vector<Helper>& people) {
-    cout << left << setw(15) << "Name" << setw(20) << "Second Name" << setw(5) << "Age" << setw(5) << "ID"
-        << setw(10) << "Parent ID" << setw(20) << "Children IDs" << "job" << endl;
-               
-                 for (auto& person : people) {
-        string children_ids = "";
-        for (auto& child : person.children) {
-            children_ids += to_string(child.id) + " ";
-    }
-        cout << left << setw(15) << Helper.P_name << setw(20) << Helper.H_name << setw(5) << Helper.age << endl;
-    }
-}
+       
 
 int main() {
 
@@ -40,9 +28,8 @@ int main() {
     time ( &rawtime );
 
     std::cin >> Helper.P_name;
-    std::cin >> Helper.P_name;
 
-    std::cout << "Hi i am a " << Helper.name << 
+    std::cout << "Hi i am a " << Helper.H_name << 
     std::cout << "Time: " <<  ctime (&rawtime);
     
     std::cout << "Code execution" << std::endl;
