ede8d5f43c57c0dba8e8d28a8f403e961f837400
Kirill <hickkan008@gmail.com>
2023-12-29 00:09:43 +0300

    modified:   helper.h 	modified:   main.cpp

--- helper.h
+++ helper.h
@@ -1,27 +1,14 @@
-#pragma warning(disable : 4996)
-
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <ctime>
 #include <vector>
-#include <iomanip>
-#include <Windows.h>
+#include <fstream>
 
+class Helper{
 
-
-class Helper
-{
 public:
-    std::string H_name;
-    std::string U_name;
-    int old;
-    void Time();
-    int getRun();
 
 private:
-    void IfRun(int HelperRun);
-    int HelperRun_;
-    void RunProgs();
 
-};
+protected:
+
+}
